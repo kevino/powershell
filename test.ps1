58972034523d50216a16c8cd8fae14d5a6239da3
Kevin <kgwells@gmail.com>
2017-02-20 10:12:06 -0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
-$fileExe = "\\van.ilsc.com\deploy\BGInfo\bginfo.exe"
-$fileConfig = "\\van.ilsc.com\deploy\BGInfo\ACAVAN-config.bgi"
+$fileExe = "\\contoso.com\deploy\BGInfo\bginfo.exe"
+$fileConfig = "\\contoso.com\deploy\BGInfo\ACAVAN-config.bgi"
 
 & $fileExe $fileConfig /timer:0 /silent /nolicprompt
